fc950e6df42f9f5e90d2ec99421454cb303f7b34
David Bonicillo <43072143+caesarbonicillo@users.noreply.github.com>
2019-11-12 18:28:53 -0800

    adds elif
    adds elif,

--- quadraticEquation.py
+++ quadraticEquation.py
@@ -10,11 +10,14 @@ def main():
     c = float(input("enter coefficient c:"))
     #run only if code is greater or qual to zero
     discrim = b * b - 4 * a *c
-    if(discrim < 0):
+    if(discrim < 0): #1, 2, 3
         print("no real roots") # should always put the default in before any real work that way it doesn't do any unnessesary work.
-    else:
+    elif(discrim ==0):#1, 2, 1
+        root = -b / (2 * a)
+        print("there is a double root at", root)
+    else:#1, 5, 6
         discRoot = math.sqrt(b * b -4 *a *c)
-        root1 = (-b + discRoot) / (2* a)
+        root1 = (-b + discRoot) / (2* a) 
         root2 = (-b - discRoot) / (2 * a)
         print ("\nThe solutions are:", root1, root2)
         
